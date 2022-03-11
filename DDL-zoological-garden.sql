6114e43c43d4ad08f26ed5b539c549b88c8a86e5
IONCIOBA <ionut.ciobanu@capgemini.com>
2022-03-11 13:54:09 +0200

    UPDATE

--- DDL-zoological-garden.sql
+++ DDL-zoological-garden.sql
@@ -1,19 +1,43 @@
--- --------------------------------------------CREATE TABLE SECTION---------------------------------------------------------
+//---------------------------------------- CREATE TABLE ANIMAL ----------------------------------------
 
+
+CREATE TABLE animal (
+id_animal INT NOT NULL,
+id_category INT NOT NULL,
+id_country INT NOT NULL,
+age INT NOT NULL,
+gender VARCHAR(10),
+PRIMARY KEY(id_animal)
+);
+
+
+//---------------------------------------- CREATE TABLE ANIMAL_CATEGORY ----------------------------------------
+
+
+CREATE TABLE animal_category (
+id_category INT NOT NULL AUTO_INCREMENT,
+type VARCHAR(20),
+id_section INT NOT NULL,
+PRIMARY KEY(id_category)
+);
+
+
+// -----------------------------------------------CREATE TABLE SECTION---------------------------------------------------------
 
 
 CREATE TABLE SECTION(
 ID_SECTION INT NOT NULL AUTO_INCREMENT,
-sectorName VARCHAR(100) NOT NULL,
+sector_name VARCHAR(100) NOT NULL,
+id_employee INT NOT NULL,
 PRIMARY KEY(ID_SECTION)
 );
 
 
--- --------------------------------------------CREATE TABLE COUNTRY---------------------------------------------------------
+// ---------------------------------------------CREATE TABLE COUNTRY---------------------------------------------------------
 
 
 CREATE TABLE COUNTRY(
 ID_COUNTRY INT NOT NULL AUTO_INCREMENT,
-countryName VARCHAR(100) NOT NULL,
+country_name VARCHAR(100) NOT NULL,
 PRIMARY KEY(ID_COUNTRY)
 );
